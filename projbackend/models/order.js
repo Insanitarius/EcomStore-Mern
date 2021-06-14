fa98fbf80522aad5255333d3d646d172fa14d509
Insanitarius <prototype.shubhodeep@gmail.com>
2021-06-14 11:56:10 +0530

    Backend work almost finished

--- projbackend/models/order.js
+++ projbackend/models/order.js
@@ -1,31 +1,39 @@
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
-const {ObjectId}= mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
 
-const ProductCartSchema=new mongoose.Schema({
-    product:{
-        type: ObjectId,
-        ref: "Product"
-    },
-    name: String,
-    count: Number,
-    price: Number
+const ProductCartSchema = new mongoose.Schema({
+  product: {
+    type: ObjectId,
+    ref: "Product",
+  },
+  name: String,
+  count: Number,
+  price: Number,
 });
 
-const ProductCart=mongoose.model("ProductCart",ProductCartSchema);
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
 
-const OrderSchema = new mongoose.Schema({
+const OrderSchema = new mongoose.Schema(
+  {
     products: [ProductCartSchema],
     transaction_id: {},
-    amount: {type:Number},
-    address:String,
+    amount: { type: Number },
+    address: String,
+    status: {
+      type: String,
+      default: "Recieved",
+      enum: ["Canceled", "Delivered", "Shipped", "Processing", "Recieved"],
+    },
     updated: Date,
     user: {
-        type: ObjectId,
-        ref: "User"
-    }
-},{timestamps:true});
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-const Order=mongoose.model("Order",OrderSchema);
+const Order = mongoose.model("Order", OrderSchema);
 
-module.exports={Order,ProductCart};
+module.exports = { Order, ProductCart };
--- projbackend/routes/order.js
+++ projbackend/routes/order.js
@@ -1,5 +1,54 @@
 const express = require("express");
 const router = express.Router();
-
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
-const { getUserById } = require("../controllers/user");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { updateStock } = require("../controllers/product");
+
+const {
+  getOrderById,
+  createOrder,
+  getAllOrders,
+  getOrderStatus,
+  updateStatus,
+} = require("../controllers/order");
+
+//params
+router.param("userId", getUserById);
+router.param("orderId", getOrderById);
+
+//Actual Routes
+//create
+router.post(
+  "/order/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  pushOrderInPurchaseList,
+  updateStock,
+  createOrder
+);
+//read
+router.get(
+  "/order/all/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getAllOrders
+);
+
+//staus of order
+router.get(
+  "/order/status/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  getOrderStatus
+);
+router.put(
+  "/order/:orderId/status/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateStatus
+);
+
+module.exports = router;
